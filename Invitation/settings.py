4e1ff5e6d887c371991d244665a15809458f0056
AlgoWala1 <120630438+AlgoWala1@users.noreply.github.com>
2025-01-08 21:57:28 +0530

    Render Ready

--- Invitation/settings.py
+++ Invitation/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ten^^80)5pvq@15#^n@v45&lqi)&2&liozl-@ifw+k3noae&a&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 

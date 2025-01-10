876e5485745359d835f581e05fcaf37487aa1de2
AlgoWala1 <120630438+AlgoWala1@users.noreply.github.com>
2025-01-10 22:22:01 +0530

    Static File Load

--- Invite/views.py
+++ Invite/views.py
@@ -6,17 +6,20 @@ def HomePage(request):
         name = data.get("Name")
         email = data.get("email")
         phone = data.get("Phone_Number")
-
-        Attending.objects.create(
-            name = name,
-            email = email,
-            phoneNumber = phone
-        )
+        if Attending.objects.filter(email = email).count() or Attending.objects.filter(phoneNumber = phone):
+            pass
+        else:
+            Attending.objects.create(
+                name = name,
+                email = email,
+                phoneNumber = phone
+            )
         
     return render(request,'invite.html')
 
 def Display(request):
     Attending.objects.filter(name__icontains='Mohin').delete()
     Attending.objects.filter(email__icontains='mohin').delete()
+    
     attend = Attending.objects.all()
     return render(request,'Display.html',context={'attending_list':attend})
